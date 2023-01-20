434122a2a490447be1cf39ce58f83456033cbf50
unknown <rahularora993676@gmail.com>
2023-01-20 09:36:47 -0800

    hooks

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Quiz from "./Component/Quiz";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{color: "Black"}}>QUIZ APPLICATION</h1>
+      <Quiz />
+    </div>
+  );
+}
